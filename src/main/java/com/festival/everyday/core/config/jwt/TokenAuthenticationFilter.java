0e32603794a301ab1d13e494fe3657dd1337a6d7
sohappytoday <kgeo6264@gmail.com>
2025-08-11 18:05:02 +0900

    feat(token): make ATTR_USER

--- src/main/java/com/festival/everyday/core/config/jwt/TokenAuthenticationFilter.java
+++ src/main/java/com/festival/everyday/core/config/jwt/TokenAuthenticationFilter.java
@@ -13,6 +13,9 @@ import java.io.IOException;
 
 @RequiredArgsConstructor
 public class TokenAuthenticationFilter extends OncePerRequestFilter {
+    public static final String ATTR_USER_ID = "auth.user_id";
+    public static final String ATTR_USER_TYPE = "auth.user_type";
+
     private final TokenProvider tokenProvider;
     private final static String HEADER_AUTHORIZATION = "Authorization";
     private final static String TOKEN_PREFIX = "Bearer ";
@@ -25,9 +28,12 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
 
         String authorizationHeader = request.getHeader(HEADER_AUTHORIZATION);
         String token = getAccessToken(authorizationHeader);
-        if(tokenProvider.validateToken(token)) {
+        if(token!= null && SecurityContextHolder.getContext().getAuthentication() == null && tokenProvider.validateToken(token)) {
             Authentication authentication = tokenProvider.getAuthentication(token);
             SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            request.setAttribute(ATTR_USER_ID, tokenProvider.getUserId(token));
+            request.setAttribute(ATTR_USER_TYPE, tokenProvider.getUserType(token));
         }
 
         filterChain.doFilter(request, response);
