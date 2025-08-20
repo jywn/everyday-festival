f95b15a78f765bfeda2db8e7ec95de98e40358f0
Jiyoonpark0208 <ascii0208@naver.com>
2025-08-20 16:19:40 +0900

    feat: notification

---
+++ src/main/java/com/festival/everyday/core/notice/controller/NotificationController.java
@@ -0,0 +1,22 @@
+package com.festival.everyday.core.notice.controller;
+
+import com.festival.everyday.core.common.config.jwt.TokenAuthenticationFilter;
+import com.festival.everyday.core.notice.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@RestController
+@RequiredArgsConstructor
+public class NotificationController {
+
+    private final NotificationService notificationService;
+
+    @GetMapping(value = "/notifications/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public SseEmitter subscribe(@RequestAttribute(name = TokenAuthenticationFilter.ATTR_USER_ID) Long userId) {
+        return notificationService.subscribe(userId);
+    }
+}
