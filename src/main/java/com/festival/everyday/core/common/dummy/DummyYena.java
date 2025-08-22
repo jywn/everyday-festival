273613f643eab4aa55bb3177f357f040c7e8eb63
sohappytoday <kgeo6264@gmail.com>
2025-08-22 17:53:23 +0900

    feat(all): dfasfasljkkl

--- src/main/java/com/festival/everyday/core/common/dummy/DummyYena.java
+++ src/main/java/com/festival/everyday/core/common/dummy/DummyYena.java
@@ -1,5 +1,8 @@
 package com.festival.everyday.core.common.dummy;
 
 public class DummyYena {
-    private Integer hello;
+    private Integer hello = 5;
+    private String hello2 = "hello";
+    private String hello3 = "hello3";
+
 }
