09421953f1fa11b7fa25fe38b7d4ca626775834a
Jongyun Won <jywon1128@gmail.com>
2025-08-24 16:39:40 +0900

    feat(dev): notice page

--- src/main/java/com/festival/everyday/core/notice/repository/NoticeRepository.java
+++ src/main/java/com/festival/everyday/core/notice/repository/NoticeRepository.java
@@ -4,6 +4,6 @@ import com.festival.everyday.core.notice.domain.Notice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
-public interface NoticeRepository extends JpaRepository<Notice, Long> {
+public interface NoticeRepository extends JpaRepository<Notice, Long>, NoticeRepositoryCustom {
     List<Notice> findByReceiverIdOrderByCreatedAtDesc(Long receiverId);
 }
--- src/main/java/com/festival/everyday/core/notice/service/NoticeQueryService.java
+++ src/main/java/com/festival/everyday/core/notice/service/NoticeQueryService.java
@@ -1,8 +1,11 @@
 package com.festival.everyday.core.notice.service;
 
+import com.festival.everyday.core.notice.domain.Notice;
 import com.festival.everyday.core.notice.dto.command.NoticeDto;
 import com.festival.everyday.core.notice.repository.NoticeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,8 +18,7 @@ public class NoticeQueryService {
 
     private final NoticeRepository noticeRepository;
 
-    public List<NoticeDto> getNotices(Long userId) {
-        return noticeRepository.findByReceiverIdOrderByCreatedAtDesc(userId).stream()
-                .map(NoticeDto::from).toList();
+    public Page<NoticeDto> getNotices(Long userId, Pageable pageable) {
+        return noticeRepository.findNoticeList(userId, pageable);
     }
 }
